b06bf914baac75ed9cbc9823bbcead020d2c06de
Alexandre <alexandre.chevalier.1@ulaval.ca>
2015-01-21 18:02:31 -0500

    upload de ex01

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ex01
+
+A minimal command-line application.
+
+
+sio-2109
+Alexandre Chevalier
---
+++ bin/main.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+
+main() {
+  String a = '1*1=1';
+  String b = '11*11=121';
+  String c = '111*111=12321';
+  String d = '1111*1111=1234321';
+  String e = '11111*11111=123454321';
+  String f = '111111*111111=12345654321';
+  String g = '1111111*1111111=1234567654321';
+  String h = '11111111*11111111=123456787654321';
+  String i = '111111111*111111111=12345678987654321﻿';
+  // impression de la pyramide avec variables String
+  print('                 ${a}');
+  print('               ${b}');
+  print('             ${c}');
+  print('           ${d}');
+  print('         ${e}');
+  print('       ${f}');
+  print('     ${g}');
+  print('   ${h}');
+  print(' ${i}');
+}
